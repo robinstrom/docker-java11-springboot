97411aa78a68ba342b9e3a1992628abdd305ef10
robinstrom <robinstrom94@gmail.com>
2018-12-10 13:38:03 +0100

    dockerizing a spring boot project written in java 11

---
+++ src/main/java/com/learning/springbootdocker/Agent.java
@@ -0,0 +1,56 @@
+package com.learning.springbootdocker;
+
+public class Agent {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Agent() {
+        super();
+    }
+
+    public Agent(Long id, String firstName, String lastName) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Agent(String firstName, String lastName) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Agent[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
